a6183af8e4e684e3137efb1371ee5ef7486cf219
LeonardoGameshow <leonciogamershow@gmail.com>
2025-02-09 14:41:33 -0600

    modify second half of index, custom main.css colors brand, create logo-inmuno class -LGS

--- Inmunolife/settings.py
+++ Inmunolife/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'Inmunolife.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('BD_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
+        'POST': env.int('DB_PORT')
     }
 }
 
--- core/models.py
+++ core/models.py
@@ -4,22 +4,27 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Contactos(models.Model):
-    nom_usuario = models.CharField(max_length=500, null=False, unique=True, verbose_name='Nombre')
+    nombre = models.CharField(max_length=100, null=False, unique=True, verbose_name='Nombre')
+    apellido = models.CharField(max_length=100, null=False, unique=True, verbose_name='Apellido')
+    email = models.EmailField(null=False, unique=True, verbose_name="Email")
+    telefono = models.IntegerField(null=True, unique=False, verbose_name='Telefono')
+    comentarios = models.TextField(null=True, verbose_name='Comentarios')
+    fecha_alta = models.DateTimeField(auto_now_add=True, verbose_name='Fecha alta')
+    estado = models.BooleanField(null=False, verbose_name='Estado')
     
     def __str__(self):
-        return self.nom_usuario
+        return self.nombre
     class Meta:
         db_table = 'contact'
-        verbose_name = 'Contacto'
+        verbose_name = 'Contactos'
         verbose_name_plural = 'Contactos'
         ordering = ['id']
 
 class Posts(models.Model):
     autor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    contacto = models.ForeignKey(Contacto, on_delete=models.CASCADE)
+    contacto = models.ForeignKey(Contactos, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200, unique=True, null=False, verbose_name='Título')
     contenido = models.TextField(null=True, verbose_name='Contenido del post')
-    imagen = models.ImageField(upload_to='post/%Y/%M/%D' null=True, verbose_name='Imagen del post')
     fecha_alta = models.DateTimeField(auto_now_add=True, verbose_name='Fecha alta')
     fecha_actualizacion = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de actualización')
 
