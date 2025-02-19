134e782a0fb21e78e43fad82511aa03d4e6e07d8
Emilio Ariza <emilioalexisag14@gmail.com>
2025-02-18 21:31:53 -0600

    Captcha, -Emix

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-from .models import Contactos
+from .models import Contactos, Register
 
 class FormContact(forms.ModelForm):
     class Meta:
         model = Contactos
         fields = '__all__'
+        
+
+# Clase para registrar usuarios
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = Register
+        fields = [
+            'name',
+            'second_lastname'
+            ]
+        
--- core/views.py
+++ core/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect #Se agrega redirect de django -Emix
 from django.http import HttpRequest
-from core.forms import FormContact
+from core.forms import FormContact, RegisterForm
+from django.contrib.auth.hashers import make_password
+from django.conf import settings #Se importa settings de la configuración  de django -Emix
+from django.contrib import messages #Se importa messages de django -Emix
+from django.contrib.auth import login
+from .models import User
+from django.core.validators import validate_integer
+from django.core.exceptions import ValidationError
+
+
+
+
+# Función para registrar usuarios intento 3
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #login(request, user)  --> Inicia sesión automáticamente
+            #return redirect("index")  # Redirige a la página principal
+    else:
+        form = RegisterForm()
+    
+
+    return render(request, "index.html")
+
 
 class FormContactView(HttpRequest):
 
     #Renderiza vista del formulario -LGS
     def index(request):
-        contact = FormContact()
+        contact = FormContact() 
         
         return render(request, "index.html", {"form":contact})
     
@@ -24,3 +49,28 @@ class FormContactView(HttpRequest):
 # Home view
 def inmunolife_home(request):
     return render(request, "index.html")
+
+
+#Función para el captcha en index -Emix
+def index_page(request):
+    if request.method == 'POST':
+        recaptcha_response = request.POST.get('g-recaptcha-response')
+        
+        data = {
+            'secret': settings.RECAPTCHA_SECERET_KEY,
+            'respone': recaptcha_response
+        }
+        verify_url = 'https://www.google.com/recaptcha/api.siteverify'
+        response = request.post(verify_url, data=data)
+        result = response.json()
+        
+        if result.get('success'):
+            
+            return redirect('success')
+        
+        else:
+            messages.error(request, 'verificación del reCAPTCHA fallida. Por favor intentelo de nuevo')
+            
+    return render (request, 'index.html')
+        
+        
